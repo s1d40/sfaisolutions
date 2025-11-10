bd5363e925ef98120c12a2787f6bc477985d0454
s1d40 <sidneifelipe9@gmail.com>
2025-11-10 14:23:23 -0300

    Deploy new frontend build

--- public/flutter_bootstrap.js
+++ public/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "108310639"
+    serviceWorkerVersion: "1667929567"
   }
 });
